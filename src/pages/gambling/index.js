d541cbd141cfa3bdbc35175499d6af293f516ab2
Gegzz <ggegeshidze95@gmail.com>
2021-03-19 06:38:27 +0400

    ifosads

--- src/pages/gambling/index.js
+++ src/pages/gambling/index.js
@@ -59,6 +59,8 @@ import battlebgsvg from './battlebg.svg'
 import battle from './battle.svg'
 import playergreen from './playergreen.svg'
 import playerred from './playerred.svg'
+import finishsvg from './finish.svg'
+import skull from './skull.svg'
 const config = require('../../config')
 
 const Flexed = (props) =>
@@ -612,7 +614,8 @@ const BattleTab = () => (
     {/* <MakeBet /> */}
     {/* <WaitingForOpponent /> */}
     {/* <ItsAMatch /> */}
-    <Battle />
+    {/* <Battle /> */}
+    <Finish />
   </div>
 )
 
@@ -911,7 +914,7 @@ const LosesWinsText = ({ win, value }) => (
   </div>
 )
 
-const FourText = ({ text, green, value, hideShield }) => (
+const FourText = ({ text, green, value }) => (
   <div
     style={{
       display: 'flex',
@@ -940,7 +943,64 @@ const FourText = ({ text, green, value, hideShield }) => (
         {value}
       </Typography>
     </Typography>
-    {!hideShield && <img alt="" src={ShieldSvg} width="68" height="140" />}
+  </div>
+)
+
+const Finish = () => (
+  <div
+    style={{
+      alignItems: 'center',
+      textAlign: 'center',
+      verticalAlign: 'middle',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      height: '100%',
+      backgroundImage: `url(${finishsvg})`,
+      backgroundSize: 'cover'
+    }}>
+    <div
+      style={{
+        backdropFilter: 'blur(1rem)',
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        borderRadius: 38,
+        height: '82%',
+        width: '82%',
+        padding: 24
+      }}>
+      <img
+        alt=""
+        src={skull}
+        height="80"
+        width="120"
+        style={{ paddingTop: 16 }}
+      />
+      <VerticalSpace />
+      <Typography
+        style={{
+          fontFamily: 'Montserrat',
+          fontWeight: 500,
+          fontSize: 16,
+          color: 'white'
+        }}>
+        {'Unfortunately, you lost this time...'.toUpperCase()}
+      </Typography>
+      <VerticalSpace />
+      <BetButton title="TRY AGAIN" />
+      <VerticalSpace />
+      <VerticalSpace />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexDirection: 'row'
+        }}>
+        <FourText text="Bet amount: " value="100$" green />
+        <FourText text="Open price: " value="55,156.69" green />
+        <FourText text="Prediction: " value="Rise" green />
+        <FourText text="Current price: " value="55,156.69" />
+      </div>
+    </div>
   </div>
 )
 
